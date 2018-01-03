d9276e5419068032bc1242585fde9d6fdb37d314
dirtymogi <35070926+dirtymogi@users.noreply.github.com>
2018-01-03 21:56:59 -0200

    add firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: 'your_api',
+  authDomain: 'your_auth',
+  databaseURL: 'your_database_url',
+  storageBucket: 'your_storage',
+  messagingSenderId: 'your_messaging'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
